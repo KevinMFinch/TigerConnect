956b7ab43ebc6fed60188ba290fbf992d9bd3eb0
Kevin <finchkevinm74@gmail.com>
2018-04-05 21:58:51 -0400

    Add comments to search

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -11,11 +11,13 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:query', (req, res) => {
-  // Query contains a number, probably a search like COS333 or COS 333
   var query = decodeURIComponent(req.params.query);
+  // Matches something like COS333
   const oneWordRegexp = /([A-Z]{3})(\d{1,3})/;
+  // Matches something like COS 333
   const twoWordRegexp = /([A-Z]{3})(\s+)(\d{1,3})/;
   var matches = []
+  // Check for first case
   if ((matches = oneWordRegexp.exec(query.toUpperCase())) !== null) {
     var department = matches[1];
     var number = matches[2];
@@ -28,6 +30,7 @@ router.get('/:query', (req, res) => {
       console.log(e);
     });
   }
+  // Check for second case
   else if ((matches = twoWordRegexp.exec(query.toUpperCase())) !== null) {
     var department = matches[1];
     var number = matches[3];
@@ -40,6 +43,7 @@ router.get('/:query', (req, res) => {
       console.log(e);
     });
   } else {
+    // If not a course search, do a regex on the fields of the Course documents.
     Course.find().or([
       {name: {$regex: query, $options: 'i'}},
       {courseNumber: {$regex: query}},
