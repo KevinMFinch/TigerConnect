677fe3bb32ec273af1029e048e786ef60b1d8639
Kevin <finchkevinm74@gmail.com>
2018-04-13 00:45:58 -0400

    Add debug statements

--- server/config.js
+++ server/config.js
@@ -1,10 +1,14 @@
 // The domain on which the app is running
+
+console.log('env', process.env);
+
 var host = process.env.HOST || 'http://localhost:5000'
 module.exports.host = host
 
 var mongoURL =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
+    process.env. MONGODB_URI ||
     'mongodb://localhost:27017/TigerConnect';
 
 console.log('url', mongoURL);
