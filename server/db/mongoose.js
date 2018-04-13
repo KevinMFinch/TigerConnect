6a80c9bf1aea907a68f4537dfd860505213ad5ea
Kevin <finchkevinm74@gmail.com>
2018-04-13 00:31:50 -0400

    Add server DB link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TigerConnect');
+// mongoose.connect('mongodb://localhost:27017/TigerConnect');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds247587.mlab.com:47587/heroku_jgzrkc10');
 
 module.exports = {mongoose};
