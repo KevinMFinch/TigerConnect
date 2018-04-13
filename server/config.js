7ceebae2b257925f85519e8310eaeb94fd66981f
Kevin <finchkevinm74@gmail.com>
2018-04-13 00:42:08 -0400

    Modularize mongo url

--- server/config.js
+++ server/config.js
@@ -1,3 +1,12 @@
 // The domain on which the app is running
 var host = process.env.HOST || 'http://localhost:5000'
 module.exports.host = host
+
+var mongoURL =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost:27017/TigerConnect';
+
+console.log('url', mongoURL);
+
+module.exports.mongoURL = mongoURL;
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+var config = require('../config');
 
 mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoURL)
 // mongoose.connect('mongodb://localhost:27017/TigerConnect');
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds247587.mlab.com:47587/heroku_jgzrkc10');
 
 module.exports = {mongoose};
