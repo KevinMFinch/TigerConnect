93724449b8229a5dbae14d832711f8bc4471f3fa
Lilly Meng <lillianlmeng@gmail.com>
2018-05-02 22:25:51 -0400

    responsive course searching

--- server/public/index.js
+++ server/public/index.js
@@ -13,14 +13,14 @@ getAllCourses = () => {
 
 var timeout = null;
 
-getSearchedCourses = query => {
+getSearchedCourses = (query, isMobile) => {
   clearTimeout(timeout);
 
   timeout = setTimeout(function () {
     var searchQuery = '/api/courses/' + query;
     fetch(searchQuery)
       .then(res => res.json())
-      .then(course => handleCourses(course));
+      .then(course => handleCourses(course, isMobile));
     }, 150);
 }
 
@@ -40,14 +40,19 @@ getPinnedCourses = query => {
       .then(courses => handlePinned(courses));
 }
 
-function handleCourses(course) {
+function handleCourses(course, isMobile) {
   var innerHTMLChange = "";
   for(var i = 0; i < course.length; i++) {
     innerHTMLChange = innerHTMLChange + "<div class=\"class slideRight\" id=\"" + course[i]['_id'] + " " + course[i]['department'] + course[i]['courseNumber'] + "\" onclick=\"searchCourseGroups(this.id); setSelected(this)\">";
     innerHTMLChange = innerHTMLChange + "<button  id=\"" + course[i]['_id'] + "\" class=\"pin mt-2 ml-auto mr-2\" onclick=\"addPinnedClass(this.id)\"><i class=\"fas fa-thumbtack\"></i></button>" + "<h5 class=\"class-title ml-4 pl-1 mt-2\">" + course[i]['department'] + course[i]['courseNumber'] + "</h5></button>";
     innerHTMLChange = innerHTMLChange + "<p class=\"hidden-sm ml-4 pl-1 mt-2 mr-4 small text-white\">" + course[i]['name'] + "</p></div>";
   }
-  document.getElementById("class-placement").innerHTML = innerHTMLChange;
+  if (isMobile) {
+    document.getElementById("class-placement-mobile").innerHTML = innerHTMLChange;
+  }
+  else {
+    document.getElementById("class-placement").innerHTML = innerHTMLChange;
+  }
 }
 
 function handlePinned(courses) {
@@ -154,8 +159,8 @@ function unpinClass(id) {
   getPinned();
 }
 
-function searchCourses(value) {
-  getSearchedCourses(value);
+function searchCourses(value, isMobile) {
+  getSearchedCourses(value, isMobile);
 }
 
 function getPinned() {
