2724767dcb70fe07c896833eb08f1fdbf4a5d1a9
ttahmed <ttahmed@princeton.edu>
2018-05-07 18:39:03 -0400

    Made title a required input in the create group form.

--- server/public/createGroup.js
+++ server/public/createGroup.js
@@ -1,20 +1,38 @@
+function validateGroup(){
+	return (document.getElementById("courseEvent-title").value.length != 0);
+}
+
 function createGroup() {
-	fetch("/api/courseEvents/", {
-		method : 'POST',
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({
-			title: document.getElementById("courseEvent-title").value,
-			advertiser: document.getElementById("netid").value,
-			time: document.getElementById("courseEvent-time").value,
-			location: document.getElementById("courseEvent-location").value,
-			description: document.getElementById("courseEvent-description").value,
-			courseID: document.getElementById("courseid").value
-		})
-	}).then(res => res.json()).then(response => console.log(response));
+	if (!validateGroup()) {
+		document.getElementById("courseEvent-title").classList.add("form-invalid");
+	}
+	else {
+		$('#createGroupModal').modal('hide');
+
+		var timeVal;
+		var locationVal;
+
+		(document.getElementById("courseEvent-time").value == "")? timeVal = "N/A" : timeVal = document.getElementById("courseEvent-time").value;
+		(document.getElementById("courseEvent-location").value == "")? locationVal = "N/A" : locationVal = document.getElementById("courseEvent-location").value;
+
+		fetch("/api/courseEvents/", {
+			method : 'POST',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				title: document.getElementById("courseEvent-title").value,
+				advertiser: document.getElementById("netid").value,
+				time: timeVal,
+				location: locationVal,
+				description: document.getElementById("courseEvent-description").value,
+				courseID: document.getElementById("courseid").value
+			})
+		}).then(res => res.json()).then(response => console.log(response));
 	document.getElementById("courseEvent-title").value = "";
 	document.getElementById("courseEvent-time").value = "";
 	document.getElementById("courseEvent-location").value = "";
 	document.getElementById("courseEvent-description").value = "";
+	document.getElementById("courseEvent-title").classList.remove("form-invalid");
+	}
 }
